83a38701199c0fc154d973ccbc3de3247a575bf5
architayadav <architanileshyadav@gmail.com>
2026-05-12 23:15:15 +0530

    Session Based

--- backend/src/main/java/com/sssms/portal/features/auth/AuthService.java
+++ backend/src/main/java/com/sssms/portal/features/auth/AuthService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
